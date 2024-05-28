e584a49c22acc20b5f74a35b40fd961927ef9e4d
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-05-27 23:45:19 -0300

    Adicionado novo modelo de Random Forest Regression

---
+++ Regressions/Random Forest Regression/Combined_Cycle_Power_Plants.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+dataset = pd.read_csv('Regressions\Multiple Linear Regression\Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+regressor = RandomForestRegressor(n_estimators=10, random_state=0)
+regressor.fit(x_train,y_train)
+y_pred = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
