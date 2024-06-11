1a8c44c011d9e94100c659f5ec7409ac5306a163
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-06-11 19:48:26 -0300

    Atualizado modelos

--- Classification/Decision Tree Classification/Social_Network_Ads.py
+++ Classification/Decision Tree Classification/Social_Network_Ads.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
 from matplotlib.colors import ListedColormap
 
@@ -18,7 +18,7 @@ sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
 
-classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier = DecisionTreeClassifier(criterion='entropy',random_state=0)
 classifier.fit(x_train, y_train)
 
 print(classifier.predict(sc.transform([[30, 87000]])))
