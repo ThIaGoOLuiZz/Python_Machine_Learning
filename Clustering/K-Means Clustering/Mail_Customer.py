3872f4b3aa1435231c2d5b7494adb44a42e37878
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-06-14 19:48:15 -0300

    Inserido metodo de Elbow (Elbow Method)

--- Clustering/K-Means Clustering/Mail_Customer.py
+++ Clustering/K-Means Clustering/Mail_Customer.py
@@ -1,7 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.cluster import KMeans
 
 dataset = pd.read_csv('Clustering\Dataset\Mall_Customers.csv')
 
 x = dataset.iloc[:, [3,4]].values
+
+wcss = []
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i,init='k-means++',random_state=42)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1,11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
