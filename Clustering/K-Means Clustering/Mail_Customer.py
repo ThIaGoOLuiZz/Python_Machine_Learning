9d2d6257d15d718999558be7181e3cc712aa707a
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-06-17 22:46:07 -0300

    Adicionado scatters para os 5 clusters

--- Clustering/K-Means Clustering/Mail_Customer.py
+++ Clustering/K-Means Clustering/Mail_Customer.py
@@ -21,3 +21,10 @@ plt.show()
 
 kmeans = KMeans(n_clusters=5,init='k-means++',random_state=42)
 y_kmeans = kmeans.fit_predict(x)
+
+plt.scatter(x[y_kmeans == 0,0], x[y_kmeans == 0,1], s = 100, c='red', label = 'Cluster 1')
+plt.scatter(x[y_kmeans == 1,0], x[y_kmeans == 1,1], s = 100, c='blue', label = 'Cluster 2')
+plt.scatter(x[y_kmeans == 2,0], x[y_kmeans == 2,1], s = 100, c='green', label = 'Cluster 3')
+plt.scatter(x[y_kmeans == 3,0], x[y_kmeans == 3,1], s = 100, c='cyan', label = 'Cluster 4')
+plt.scatter(x[y_kmeans == 4,0], x[y_kmeans == 4,1], s = 100, c='red', label = 'Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label= 'Centroids')
